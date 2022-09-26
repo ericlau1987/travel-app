5c823b81844ab3da1a3b7e0ef8b1e8554576e2c3
Eric Yifu Liu <46115029+ericlau1987@users.noreply.github.com>
2022-09-27 00:07:27 +1000

    updates

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,12 @@
 import { handleSubmit } from './js/formHandler'
 
-window.addEventListener('DOMContentLoaded', () => {
-    const submitButton = document.getElementById('submitButton');
-    submitButton.addEventListener('click', handleSubmit);
-  })
+// window.addEventListener('DOMContentLoaded', () => {
+//     const submitButton = document.getElementById('generate');
+//     submitButton.addEventListener('click', handleSubmit);
+//   })
+
+const submitButton = document.getElementById('generate');
+submitButton.addEventListener('click', handleSubmit);
 
 alert("I EXIST")
 console.log("CHANGE!!");
